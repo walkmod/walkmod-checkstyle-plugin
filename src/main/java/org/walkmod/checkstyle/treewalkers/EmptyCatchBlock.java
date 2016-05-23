5951af16d2ea69daa2b446ec686abc8b76208377
rpau <rpau@ac.upc.edu>
2016-05-23 18:13:20 +0200

    removing empty catch blocks

--- src/main/java/org/walkmod/checkstyle/treewalkers/EmptyCatchBlock.java
+++ src/main/java/org/walkmod/checkstyle/treewalkers/EmptyCatchBlock.java
@@ -1,83 +1,91 @@
 package org.walkmod.checkstyle.treewalkers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.walkmod.checkstyle.visitors.AbstractCheckStyleRule;
 import org.walkmod.javalang.ast.BlockComment;
 import org.walkmod.javalang.ast.Comment;
 import org.walkmod.javalang.ast.CompilationUnit;
-import org.walkmod.javalang.ast.LineComment;
 import org.walkmod.javalang.ast.stmt.BlockStmt;
 import org.walkmod.javalang.ast.stmt.CatchClause;
 import org.walkmod.javalang.ast.stmt.TryStmt;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class EmptyCatchBlock<A> extends AbstractCheckStyleRule<A> {
 
-    private String commentFormat = " Intentionally blank ";
+   private String commentFormat = " Intentionally blank ";
 
-    private List<Comment> defaultCatchComments = new ArrayList<Comment>();
+   private List<Comment> defaultCatchComments = new ArrayList<Comment>();
 
-    public String getCommentFormat() {
-        return commentFormat;
-    }
+   public String getCommentFormat() {
+      return commentFormat;
+   }
 
-    public void setCommentFormat(String commentFormat) {
-        this.commentFormat = commentFormat;
-    }
+   public void setCommentFormat(String commentFormat) {
+      this.commentFormat = commentFormat;
+   }
 
-    @Override
-    public void visit(TryStmt n, A ctx) {
-        List<CatchClause> catchList = n.getCatchs();
-        for (CatchClause catchClause : catchList) {
+   @Override
+   public void visit(TryStmt n, A ctx) {
+
+      BlockStmt block = n.getTryBlock();
+      if (block == null || block.getStmts() == null || block.getStmts().isEmpty()) {
+         n.remove();
+      } else {
+
+         List<CatchClause> catchList = n.getCatchs();
+         for (CatchClause catchClause : catchList) {
             if (catchClause.getCatchBlock().getStmts() == null) {
-                addDefaultComment(catchClause.getCatchBlock());
+               addDefaultComment(catchClause.getCatchBlock());
             }
-        }
-        super.visit(n, ctx);
-    }
+         }
+         super.visit(n, ctx);
+      }
+   }
 
-    @Override
-    public void visit(CompilationUnit cu, A ctx) {
-        super.visit(cu, ctx);
-        List<Comment> cuComments = cu.getComments();
-        if (!defaultCatchComments.isEmpty()) {
-            if (cuComments != null && !cuComments.isEmpty()) {
-                addOnlyNotOverlappedComments(cuComments);
-            } else {
-                cu.setComments(defaultCatchComments);
+   @Override
+   public void visit(CompilationUnit cu, A ctx) {
+      super.visit(cu, ctx);
+      List<Comment> cuComments = cu.getComments();
+      if (!defaultCatchComments.isEmpty()) {
+         if (cuComments != null && !cuComments.isEmpty()) {
+            addOnlyNotOverlappedComments(cuComments);
+         } else {
+            cu.setComments(defaultCatchComments);
+         }
+      }
+   }
+
+   private void addOnlyNotOverlappedComments(List<Comment> cuComments) {
+      List<Comment> commentsToAdd = new ArrayList<Comment>();
+      for (Comment catchComment : defaultCatchComments) {
+         for (Comment cuComment : cuComments) {
+            if (!areOverlappedComments(cuComment, catchComment)) {
+               commentsToAdd.add(catchComment);
             }
-        }
-    }
+         }
+      }
+      cuComments.addAll(commentsToAdd);
+   }
 
-    private void addOnlyNotOverlappedComments(List<Comment> cuComments) {
-        List<Comment> commentsToAdd = new ArrayList<Comment>();
-        for (Comment catchComment : defaultCatchComments) {
-            for (Comment cuComment : cuComments) {
-                if (!areOverlappedComments(cuComment, catchComment)) {
-                    commentsToAdd.add(catchComment);
-                }
-            }
-        }
-        cuComments.addAll(commentsToAdd);
-    }
+   private boolean areOverlappedComments(Comment cu1, Comment cu2) {
+      return isCommentStartedInsideComment(cu1, cu2) || isCommentStartedInsideComment(cu2, cu1);
+   }
 
-    private boolean areOverlappedComments(Comment cu1, Comment cu2) {
-        return isCommentStartedInsideComment(cu1, cu2) || isCommentStartedInsideComment(cu2, cu1);
-    }
+   private boolean isCommentStartedInsideComment(Comment cu1, Comment cu2) {
+      return (cu1.getBeginLine() > cu2.getBeginLine()
+            || cu1.getBeginLine() == cu2.getBeginLine() && cu1.getBeginColumn() >= cu2.getBeginColumn())
+            && (cu1.getBeginLine() < cu2.getEndLine()
+                  || cu1.getBeginLine() == cu2.getEndLine() && cu1.getEndColumn() <= cu2.getEndColumn());
+   }
 
-    private boolean isCommentStartedInsideComment(Comment cu1, Comment cu2) {
-        return (cu1.getBeginLine() > cu2.getBeginLine() || cu1.getBeginLine() == cu2.getBeginLine() && cu1.getBeginColumn() >= cu2.getBeginColumn())
-                && (cu1.getBeginLine() < cu2.getEndLine() || cu1.getBeginLine() == cu2.getEndLine() && cu1.getEndColumn() <= cu2.getEndColumn());
-    }
-
-    private void addDefaultComment(BlockStmt catchBlock) {
-        BlockComment blockComment = new BlockComment();
-        blockComment.setContent(commentFormat);
-        blockComment.setBeginLine(catchBlock.getBeginLine());
-        blockComment.setEndLine(catchBlock.getEndLine());
-        blockComment.setBeginColumn(catchBlock.getBeginColumn());
-        blockComment.setEndColumn(catchBlock.getEndColumn());
-        defaultCatchComments.add(blockComment);
-    }
+   private void addDefaultComment(BlockStmt catchBlock) {
+      BlockComment blockComment = new BlockComment();
+      blockComment.setContent(commentFormat);
+      blockComment.setBeginLine(catchBlock.getBeginLine());
+      blockComment.setEndLine(catchBlock.getEndLine());
+      blockComment.setBeginColumn(catchBlock.getBeginColumn());
+      blockComment.setEndColumn(catchBlock.getEndColumn());
+      defaultCatchComments.add(blockComment);
+   }
 }
--- src/test/java/org/walkmod/checkstyle/treewalkers/EmptyCatchBlockTest.java
+++ src/test/java/org/walkmod/checkstyle/treewalkers/EmptyCatchBlockTest.java
@@ -1,48 +1,65 @@
 package org.walkmod.checkstyle.treewalkers;
 
-import junit.framework.TestCase;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.walkmod.javalang.ASTManager;
 import org.walkmod.javalang.ast.Comment;
 import org.walkmod.javalang.ast.CompilationUnit;
+import org.walkmod.javalang.ast.body.MethodDeclaration;
 
-import java.util.List;
+import junit.framework.TestCase;
 
 public class EmptyCatchBlockTest extends TestCase {
 
-    @Test
-    public void test_shouldAddComment_whenEmptyCatch() throws Exception {
-        CompilationUnit cu = ASTManager.parse("public class Foo { public void bar() { try{} catch (Exception e ){}}}");
-        EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
-        cu.accept(visitor, null);
-        List<Comment> comments = cu.getComments();
-        Assert.assertTrue(comments.size() == 1);
-    }
+   @Test
+   public void test_shouldAddComment_whenEmptyCatch() throws Exception {
+      CompilationUnit cu = ASTManager
+            .parse("public class Foo { public void bar() { try{ bar(); } catch (Exception e ){}}}");
+      EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
+      cu.accept(visitor, null);
+      List<Comment> comments = cu.getComments();
+      Assert.assertTrue(comments.size() == 1);
+   }
 
-    @Test
-    public void test_shouldDoNothing_whenNotEmptyCatch() throws Exception {
-        CompilationUnit cu = ASTManager.parse("public class Foo { public void bar() { try{} catch (Exception e ){return;}}}");
-        EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
-        cu.accept(visitor, null);
-        List<Comment> comments = cu.getComments();
-        Assert.assertTrue(comments== null);
-    }
+   @Test
+   public void test_shouldDoNothing_whenNotEmptyCatch() throws Exception {
+      CompilationUnit cu = ASTManager
+            .parse("public class Foo { public void bar() { try{} catch (Exception e ){return;}}}");
+      EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
+      cu.accept(visitor, null);
+      List<Comment> comments = cu.getComments();
+      Assert.assertTrue(comments == null);
+   }
 
-    @Test
-    public void test_shouldDoNothing_whenCatchWithOnlyComments() throws Exception {
-        CompilationUnit cu = ASTManager.parse("public class Foo { public void bar() { try{} catch (Exception e ){/*Hola*/}}}");
-        EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
-        cu.accept(visitor, null);
-        List<Comment> comments = cu.getComments();
-        Assert.assertTrue(comments.size() == 1);
-    }
-    @Test
-    public void test_shouldAddComment_whenEmptyCatchAndMoreComments() throws Exception {
-        CompilationUnit cu = ASTManager.parse("public class Foo { public void bar() { try{/*Hola*/} catch (Exception e ){}}}");
-        EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
-        cu.accept(visitor, null);
-        List<Comment> comments = cu.getComments();
-        Assert.assertTrue(comments.size() == 2);
-    }
+   @Test
+   public void test_shouldDoNothing_whenCatchWithOnlyComments() throws Exception {
+      CompilationUnit cu = ASTManager
+            .parse("public class Foo { public void bar() { try{} catch (Exception e ){/*Hola*/}}}");
+      EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
+      cu.accept(visitor, null);
+      List<Comment> comments = cu.getComments();
+      Assert.assertTrue(comments.size() == 1);
+   }
+
+   @Test
+   public void test_shouldAddComment_whenEmptyCatchAndMoreComments() throws Exception {
+      CompilationUnit cu = ASTManager
+            .parse("public class Foo { public void bar() { try{bar(); /*hello*/} catch (Exception e ){}}}");
+      EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
+      cu.accept(visitor, null);
+      List<Comment> comments = cu.getComments();
+      Assert.assertTrue(comments.size() == 2);
+   }
+
+   @Test
+   public void test_shouldRemove_emptyCatch() throws Exception {
+      CompilationUnit cu = ASTManager.parse("public class Foo { public void bar() { try{} catch (Exception e ){}}}");
+      EmptyCatchBlock<?> visitor = new EmptyCatchBlock<Object>();
+      cu.accept(visitor, null);
+
+      MethodDeclaration md = (MethodDeclaration) cu.getTypes().get(0).getMembers().get(0);
+      Assert.assertTrue(md.getBody().getStmts().isEmpty());
+   }
 }
