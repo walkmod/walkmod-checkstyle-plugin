343ed77a102b6315322d5e8e15777454727ea897
Raquel Pau <rpau@users.noreply.github.com>
2018-03-29 20:49:36 +0200

    Fix AvoidStarImports for anonymous classes (#5)

--- src/test/java/org/walkmod/checkstyle/treewalkers/AvoidStarImportTest.java
+++ src/test/java/org/walkmod/checkstyle/treewalkers/AvoidStarImportTest.java
@@ -23,4 +23,13 @@ public class AvoidStarImportTest extends SemanticTest{
       Assert.assertEquals(1, cu.getImports().size());
       Assert.assertEquals("java.util.List", cu.getImports().get(0).getName().toString());
    }
+
+   @Test
+   public void testInnerClasses() throws Exception {
+      CompilationUnit cu = compile("import java.util.*; public class Foo{ Map.Entry e; }");
+      AvoidStarImport<?> visitor = new AvoidStarImport<Object>();
+      cu.accept(visitor, null);
+      Assert.assertEquals(1, cu.getImports().size());
+      Assert.assertEquals("java.util.Map", cu.getImports().get(0).getName().toString());
+   }
 }
